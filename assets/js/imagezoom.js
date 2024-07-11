08903ad36c5de26489afc06e5b1bc0cd5ac71153
cawald3 <cawald3@gatech.edu>
2024-07-10 21:23:58 -0400

    Image zoom feature

---
+++ assets/js/imagezoom.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modal-img");
+    var closeBtn = document.getElementsByClassName("close")[0];
+
+    document.querySelectorAll('.zoomable').forEach(img => {
+        img.onclick = function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        }
+    });
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
